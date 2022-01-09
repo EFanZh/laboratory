08f6c3796adb2cb4c167a8a24444fd79a52adc6d
EFanZh <efanzh@gmail.com>
2022-01-09 21:28:43 +0800

    Add blanket trait implementations

--- src/functions/mod.rs
+++ src/functions/mod.rs
@@ -14,3 +14,32 @@ pub trait FnMut1<T>: FnOnce1<T> {
 pub trait Fn1<T>: FnMut1<T> {
     fn call(&self, arg: T) -> Self::Output;
 }
+
+impl<T, R> FnOnce1<T> for T
+where
+    T: FnOnce(T) -> R,
+{
+    type Output = R;
+
+    fn call_once(self, arg: T) -> Self::Output {
+        self(arg)
+    }
+}
+
+impl<T, R> FnMut1<T> for T
+where
+    T: FnMut(T) -> R,
+{
+    fn call_mut(&mut self, arg: T) -> Self::Output {
+        self(arg)
+    }
+}
+
+impl<T, R> Fn1<T> for T
+where
+    T: Fn(T) -> R,
+{
+    fn call(&self, arg: T) -> Self::Output {
+        self(arg)
+    }
+}
