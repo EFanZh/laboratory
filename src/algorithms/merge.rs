85507bd82f67ce3edee06d21cae61c51ac8a5878
EFanZh <efanzh@gmail.com>
2025-05-01 13:50:42 +0800

    Update merging algorithm

--- src/algorithms/merge.rs
+++ src/algorithms/merge.rs
@@ -1,68 +1,81 @@
-pub fn merge_1<I>(mut iter_1: I, mut iter_2: I, mut f: impl FnMut(I::Item))
-where
+pub fn merge_1<'a, I>(
+    mut left_iter: I,
+    mut right_iter: I,
+    mut buffer_iter: impl Iterator<Item = &'a mut I::Item>,
+) where
     I: Iterator,
-    I::Item: Ord,
+    I::Item: Ord + 'a,
 {
-    if let Some(mut left) = iter_1.next() {
-        'outer: loop {
-            if let Some(right) = iter_2.next() {
-                while left <= right {
-                    f(left);
+    if let Some(mut left) = left_iter.next() {
+        let mut write = |value| *buffer_iter.next().unwrap() = value;
 
-                    if let Some(next_left) = iter_1.next() {
-                        left = next_left;
-                    } else {
-                        left = right;
+        'outer: for right in right_iter.by_ref() {
+            while left <= right {
+                write(left);
 
-                        break 'outer;
-                    }
+                if let Some(next_left) = left_iter.next() {
+                    left = next_left;
+                } else {
+                    left = right;
+                    left_iter = right_iter;
+
+                    break 'outer;
                 }
-
-                f(right);
-            } else {
-                iter_2 = iter_1;
-
-                break;
             }
+
+            write(right);
         }
 
-        f(left);
+        write(left);
+    } else {
+        left_iter = right_iter;
     }
 
-    iter_2.for_each(f);
+    buffer_iter
+        .zip(left_iter)
+        .for_each(|(target, value)| *target = value);
 }
 
-pub fn merge_2<I1, I2>(mut iter_1: I1, mut iter_2: I1, mut f: impl FnMut(I1::Item))
-where
+pub fn merge_2<'a, I1, I2>(
+    mut left_iter: I1,
+    mut right_iter: I1,
+    mut buffer: impl Iterator<Item = &'a mut I1::Item>,
+) where
     I1: Iterator,
     I2: Iterator<Item = I1::Item>,
-    I1::Item: Ord,
+    I1::Item: Ord + 'a,
 {
-    if let Some(mut left) = iter_1.next() {
-        'outer: loop {
-            if let Some(right) = iter_2.next() {
-                while left <= right {
-                    f(left);
+    'block: {
+        if let Some(mut left) = left_iter.next() {
+            let mut write = |value| *buffer.next().unwrap() = value;
 
-                    if let Some(next_left) = iter_1.next() {
+            for right in right_iter.by_ref() {
+                while left <= right {
+                    write(left);
+
+                    if let Some(next_left) = left_iter.next() {
                         left = next_left;
                     } else {
-                        f(right);
+                        write(right);
 
-                        break 'outer;
+                        break 'block;
                     }
                 }
 
-                f(right);
-            } else {
-                f(left);
-
-                iter_1.for_each(f);
-
-                return;
+                write(right);
             }
+
+            write(left);
+
+            buffer
+                .zip(left_iter)
+                .for_each(|(target, value)| *target = value);
+
+            return;
         }
     }
 
-    iter_2.for_each(f);
+    buffer
+        .zip(right_iter)
+        .for_each(|(target, value)| *target = value);
 }
